899aae417a4b7ef5f2bd5c2b1a88285fe023d0f5
Phoenixx <79042077+a-jeez@users.noreply.github.com>
2025-09-17 16:00:14 +0700

    Opsi lanjut & tidak, serta validasi variabel PilihanAceng

--- gunting batu kertas.cpp
+++ gunting batu kertas.cpp
@@ -1,42 +1,66 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main() {
+
 	string PilihanAceng;
 	string PilihanKomputer;
 	int randomKomputer;
 	
-	cout << "Masukkan pilihan gunting/batu/kertas: " << endl;
-	cin >> PilihanAceng;
-
-	//printf("Masukkan pilihan gunting/batu/kertas: "); gets(PilihanAceng);
-	randomKomputer = rand() % 3;
+	while(true) {
+		cout << "Masukkan pilihan gunting/batu/kertas: " << endl;
+		cin >> PilihanAceng;
+		srand(time(0));
+		
+		while(PilihanAceng != "Gunting" && PilihanAceng != "Batu" && PilihanAceng != "Kertas") {
+			cout << "Masukkan pilihan gunting/batu/kertas: " << endl;
+			cin >> PilihanAceng;
+		}
+		randomKomputer = rand() % 3;
+		
+		if(randomKomputer == 0) {
+			PilihanKomputer = "Gunting";
+		} else if(randomKomputer == 1) {
+			PilihanKomputer = "Batu";
+		} else if(randomKomputer == 2) {
+			PilihanKomputer = "Kertas";
+		}
 	
-	if(randomKomputer == 0) {
-		PilihanKomputer = "Gunting";
-	} else if(randomKomputer == 1) {
-		PilihanKomputer = "Batu";
-	} else if(randomKomputer == 2) {
-		PilihanKomputer = "Kertas";
-	}
-
-	sleep(2);	
+		sleep(2);	
+		
+		cout << "Aceng memilih " << PilihanAceng << ", sedangkan " << "Komputer memilih " << PilihanKomputer << endl;
 	
-	cout << "Aceng memilih " << PilihanAceng << ", sedangkan " << "Komputer memilih " << PilihanKomputer << endl;
+		if(PilihanAceng == PilihanKomputer) {
+			printf("\nMaka dengan ini, dinyatakan seri\n");	
+		}
+		else if
+			((PilihanAceng == "Gunting" && PilihanKomputer == "Kertas") ||
+			(PilihanAceng == "Batu" && PilihanKomputer == "Gunting") ||
+			(PilihanAceng == "Kertas" && PilihanKomputer == "Batu"))
+		{
+			cout << "Maka aceng menang\n" << endl;	
+		}
+		else {
+			cout << "Maka aceng kalah\n" << endl;
+		}
+		
+		string konfirmLanjut;
+		cout << "Apakah anda ingin lanjut? (y/n)";
+		cin >> konfirmLanjut;
+		
+		while(konfirmLanjut != "y" && konfirmLanjut != "n") {
+			cout << "Apakah anda ingin lanjut? (y/n)";
+			cin >> konfirmLanjut;
+		}
+		if(konfirmLanjut == "y") {
+			continue;
+		} else if(konfirmLanjut == "n") {
+			break;
+		}
+	}
 
-	if(PilihanAceng == PilihanKomputer) {
-		printf("\nMaka dengan ini, dinyatakan seri");	
-	}
-	else if
-		((PilihanAceng == "Gunting" && PilihanKomputer == "Kertas") ||
-		(PilihanAceng == "Batu" && PilihanKomputer == "Gunting") ||
-		(PilihanAceng == "Kertas" && PilihanKomputer == "Batu"))
-	{
-		cout << "Maka aceng menang" << endl;	
-	}
-	else {
-		cout << "Maka aceng kalah" << endl;
-	}
 }
