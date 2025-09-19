629e022eb10b9092557fde4ee13ef4c3dc295ecb
Phoenixx <79042077+a-jeez@users.noreply.github.com>
2025-09-19 15:14:38 +0700

    Add files via upload

---
+++ PiramidSegitiga.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "- Program Piramid Bintang Menggunakan Rumus dr Matematika\n";
+	
+	int tinggi_segitiga, i, j, k;
+	cout << "Input tinggi segitiga: ";
+	cin >> tinggi_segitiga;
+	
+	cout << "\n";
+	
+	for(i = 1; i <= tinggi_segitiga; i++) {
+		for(j = 1; j <= tinggi_segitiga - i; j++) {
+			cout << " ";
+		}
+		for(k = 1; k <= i; k++) {
+			cout << "* ";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
