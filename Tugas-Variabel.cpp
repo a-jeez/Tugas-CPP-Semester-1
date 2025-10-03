43ad1aab385dbbde6ac63d6a57bb04193b738028
Phoenixx <79042077+a-jeez@users.noreply.github.com>
2025-10-03 18:12:18 +0700

    Add files via upload

---
+++ Tugas-Variabel.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+main() {
+	int nilai_1, nilai_2, hasil;
+	
+	cout << "Program Pembagian Bilangan Bulat" << endl;
+	cout << "Masukkan bilangan pertama: ";
+	cin >> nilai_1;
+	
+	cout << "Masukkan bilangan kedua: ";
+	cin >> nilai_2;
+	
+	hasil = nilai_1/nilai_2;
+	
+	cout << "Hasil dari pembagian bilangan tersebut adalah: " << hasil;
+	return 0;
+}
